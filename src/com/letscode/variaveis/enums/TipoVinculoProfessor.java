9a3258ec4169093a6a34b1288ccfb3bd474095f2
Giovanni <gvn.gentile@gmail.com>
2021-12-02 08:46:19 -0300

    feat: adicionando enum

--- src/com/letscode/variaveis/enums/TipoVinculoProfessor.java
+++ src/com/letscode/variaveis/enums/TipoVinculoProfessor.java
@@ -1,17 +1,3 @@
 package com.letscode.variaveis.enums;
 
-public enum TipoVinculoProfessor {
-    INTEGRAL("Integral"), MEIO_PERIODO("Meio período");
-
-
-    private String descricao;
-
-    TipoVinculoProfessor(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-}
+public enum TipoVinculoProfessor { INTEGRAL, MEIO_PERIODO }

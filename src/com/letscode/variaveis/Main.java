c067fe2933e3ea6f82eff271519aca4906500fee
Giovanni <gvn.gentile@gmail.com>
2021-12-03 20:50:11 -0300

    feat: adicionando scans

--- src/com/letscode/variaveis/Main.java
+++ src/com/letscode/variaveis/Main.java
@@ -2,36 +2,46 @@ package com.letscode.variaveis;
 
 import com.letscode.variaveis.enums.TipoVinculoProfessor;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Variáveis para armazenar texto / caracteres
-        String nomeProfessor = "Giovanni";
-        char inicialNomeProfessor = 'G';
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insira o nome do professor: ");
+        String nomeProfessor = scanner.nextLine();
+
+        char inicialNomeProfessor = nomeProfessor.charAt(0);
 
         // Variáveis para guardar números inteiros
         // byte, short
-        int idadeProfessor = 29;
+        System.out.print("Digite a idade do professor: ");
+        Scanner scanIdade = new Scanner(System.in);
+        int idadeProfessor = scanIdade.nextInt();
         long numeroCadastroProfessor = 5558790L;
 
         // Variáveis para guardar números decimais
         // double, float
         // para fins didáticos vamos de float... mas iremos falar de BigDecimal
         // double slarioProfessor = 1000d;
-        float salarioProfessor = 1000.5f;
+       System.out.println("Digite o salário: ");
+       float salarioProfessor = scanner.nextFloat();
 
         // has
-        boolean isProfessorAuxiliar = false;
+        System.out.print("O professor é auxiliar? ");
+        boolean isProfessorAuxiliar = scanner.nextBoolean();
 
         TipoVinculoProfessor tipoVinculoProfessor = TipoVinculoProfessor.MEIO_PERIODO;
 
         System.out.println("O nome do professor é " + nomeProfessor);
+        System.out.println("O nome do professor é " + inicialNomeProfessor);
         System.out.println("A idade do professor é " + idadeProfessor);
         System.out.println("O cadastro do professor é " + numeroCadastroProfessor);
         System.out.printf("O salário do professor é %.2f %n", salarioProfessor);
-        System.out.println("O vínculo do professor é "
-                + tipoVinculoProfessor.toString().replace('_', ' '));
+        System.out.println("O vínculo do professor é " + tipoVinculoProfessor.toString().replace('_', ' '));
+        System.out.println("O professor é auxiliar? " + isProfessorAuxiliar);
 
         final float VALOR_MINIMO_HORA_AULA = 45f;
 
